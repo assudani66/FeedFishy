439b9901e86c71340898c3f6e03e7d8dc065515d
assudani66 <assudani66@gmail.com>
2023-02-20 23:51:30 +0530

    Finish All

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import * as THREE from 'three'
-import { Button, FishGauge, Logo, ScoreDisplay } from './UIcomponents'
-import { FeedContextProvider, FishPositionProvider, ScoreContextProvider, WidthContextProvider } from './feedContext'
+import { Button, FishGauge, Logo, ScoreDisplay, Tutorial } from './UIcomponents'
+import { FeedContextProvider, FishElementProvider, FishPositionProvider, InitialFishPositionProvider, scoreContext, ScoreContextProvider, WidthContextProvider } from './feedContext'
+import { useContext, useEffect, useState } from 'react'
 
 const CameraLocation = {x: 0.0003941118749341708, y: 0.6759516532057666, z: 2.007868876840685}
+
 export default function App() {
+
+
+
     return (
         <div className='gridContainer' >
             <ScoreContextProvider>
             <WidthContextProvider>
                 <FishPositionProvider>
-            <FeedContextProvider>
+                <FeedContextProvider>
+                <FishElementProvider>
+                <InitialFishPositionProvider>
+                <Tutorial/>
                 <Logo name="Feed Fishy" />
+
                 <div className="ThreeCanvas">
                 <ScoreDisplay/>
                     <Canvas
@@ -28,17 +37,21 @@ export default function App() {
                             near: 0.1,
                             far: 200,
                             position: [CameraLocation.x, CameraLocation.y, CameraLocation.z]
-                        }}
-                        
-                    >
+                        }}>
                         <Experience />
                     </Canvas>
                 </div>
                 <FishGauge/>
-                <Button name="Feed me" />
-            </FeedContextProvider>
-            </FishPositionProvider>
-            </WidthContextProvider>
+
+                    <Button name="Feed me"/>
+                    
+
+                    
+                    </InitialFishPositionProvider>
+                    </FishElementProvider>
+                    </FeedContextProvider>
+                    </FishPositionProvider>
+                </WidthContextProvider>
             </ScoreContextProvider>
         </div>
     )
